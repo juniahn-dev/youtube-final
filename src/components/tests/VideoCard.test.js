99f1803c6ae8019021bb904434856cf646bbd450
juniahn <gud0415@gmail.com>
2024-03-19 14:18:13 +0900

    [chore] Add route test

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,8 +1,9 @@
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 
-import { MemoryRouter } from "react-router-dom";
 import VideoCard from "../VideoCard";
 import { formatAgo } from "../../util/date";
+import userEvent from "@testing-library/user-event";
 
 describe("VideoCard", () => {
   const video = {
@@ -35,5 +36,28 @@ describe("VideoCard", () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it("navigates to detailed video page with video state when clicked", () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const card = screen.getByRole("listitem");
+    userEvent.click(card);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });
