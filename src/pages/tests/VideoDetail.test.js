be60582e2d431edee5591b0eada9359188612ff7
juniahn <gud0415@gmail.com>
2024-03-27 12:48:31 +0900

    [chore] Add page component test

---
+++ src/pages/tests/VideoDetail.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+
+import ChannelInfo from "../../components/ChannelInfo";
+import RelatedVideos from "../../components/RelatedVideos";
+import { Route } from "react-router-dom";
+import VideoDetail from "../VideoDetail";
+import { fakeVideo } from "../../test/videos";
+import { withRouter } from "../../test/utils";
+
+jest.mock("../../components/ChannelInfo");
+jest.mock("../../components/RelatedVideos");
+
+describe("VideoDetail", () => {
+  afterEach(() => {
+    ChannelInfo.mockReset();
+    RelatedVideos.mockReset();
+  });
+
+  it("renders video item details", () => {
+    render(
+      withRouter(<Route path="/" element={<VideoDetail />} />, {
+        pathname: "/",
+        state: { video: fakeVideo },
+        key: "fake-key",
+      })
+    );
+
+    const { title, channelId, channelTitle } = fakeVideo.snippet;
+
+    expect(screen.getByTitle(title)).toBeInTheDocument();
+    expect(RelatedVideos.mock.calls[0][0]).toStrictEqual({ id: fakeVideo.id });
+    expect(ChannelInfo.mock.calls[0][0]).toStrictEqual({
+      id: channelId,
+      name: channelTitle,
+    });
+  });
+});
---
+++ src/pages/tests/Videos.test.js
@@ -0,0 +1,70 @@
+import { fakeVideo, fakeVideos } from "../../test/videos";
+import { render, screen, waitFor } from "@testing-library/react";
+import { withAllContexts, withRouter } from "../../test/utils";
+
+import { Route } from "react-router-dom";
+import Videos from "../Videos";
+
+describe("Videos", () => {
+  const fakeYoutube = {
+    search: jest.fn(),
+  };
+
+  beforeEach(() => {
+    fakeYoutube.search.mockImplementation((keyword) => {
+      return keyword ? [fakeVideo] : fakeVideos;
+    });
+  });
+
+  afterEach(() => fakeYoutube.search.mockReset());
+
+  it("renders all videos when keyword is not specified", async () => {
+    renderWithPath("/");
+
+    expect(fakeYoutube.search).toHaveBeenCalledWith(undefined);
+    await waitFor(() =>
+      expect(screen.getAllByRole("listitem")).toHaveLength(fakeVideos.length)
+    );
+  });
+
+  it("when keyword is specified, renders search results", async () => {
+    const searchKeyword = "fake-keyword";
+    renderWithPath(`/${searchKeyword}`);
+
+    expect(fakeYoutube.search).toHaveBeenCalledWith(searchKeyword);
+    await waitFor(() =>
+      expect(screen.getAllByRole("listitem")).toHaveLength(1)
+    );
+  });
+
+  it("renders loading state when items are being fetched", async () => {
+    renderWithPath("/");
+
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("renders error state when fetching items fails", async () => {
+    fakeYoutube.search.mockImplementation(async () => {
+      throw new Error("error");
+    });
+
+    renderWithPath("/");
+
+    await screen.findByText("Something is wrong 😖");
+  });
+
+  function renderWithPath(path) {
+    return render(
+      withAllContexts(
+        withRouter(
+          <>
+            <Route path="/" element={<Videos />} />
+            <Route path="/:keyword" element={<Videos />} />
+          </>,
+          path
+        ),
+        fakeYoutube
+      )
+    );
+  }
+});
