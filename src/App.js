dd32bedb1a82a984518fbe6c5c1456d60901d226
juniahn <gud0415@gmail.com>
2024-03-28 16:55:14 +0900

    [chore] Make first e2e test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Outlet } from 'react-router-dom';
-import SearchHeader from './components/SearchHeader';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { YoutubeApiProvider } from './context/YoutubeApiContext';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
+import { YoutubeApiProvider } from "./context/YoutubeApiProvider";
 
 const queryClient = new QueryClient();
 
